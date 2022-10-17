239087e478531ee198ad35ab2f34f21ca7aec06d
CJ Buys <cjstrattonbuys@gmail.com>
2022-10-17 18:54:10 -0400

    Edited homepage code and styles

--- front-page.php
+++ front-page.php
@@ -16,14 +16,13 @@ get_header(); ?>
 				<h1><?php the_title(); ?></h1>
 				<?php the_content(); ?>
 				<div class="social-btns">  <!-- populate or remove as many of these as you want -->
-					<a href="https://twitter.com/cjstrattonbuys" class="soc-icon tw"><span class="screen-reader-text">Twitter</span></a>
-					<a href="https://www.facebook.com/cjstrattonbuys/" class="soc-icon fb"><span class="screen-reader-text">Facebook</span></a>
 					<a href="https://www.linkedin.com/in/cjstrattonbuys/" class="soc-icon ln"><span class="screen-reader-text">LinkedIn</span></a>
+					<a href="https://twitter.com/cjstrattonbuys" class="soc-icon tw"><span class="screen-reader-text">Twitter</span></a>
+					<a href="https://www.instagram.com/cjstrattonbuys/" class="soc-icon in"><span class="screen-reader-text">Instagram</span></a>
 					<a href="https://dribbble.com/cjstrattonbuys" class="soc-icon db"><span class="screen-reader-text">Dribble</span></a>
 					<a href="https://github.com/cjstrattonbuys" class="soc-icon gh"><span class="screen-reader-text">GitHub</span></a>
-					<a href="https://www.instagram.com/cjstrattonbuys/" class="soc-icon in"><span class="screen-reader-text">Instagram</span></a>
 				</div>
-				<a href="<?php echo site_url('/blog/'); ?>" class="btn">View My Blog</a>
+				<a href="<?php echo site_url('/portfolio/'); ?>" class="btn">View My Portfolio</a>
 			<?php endwhile; ?>
 		</div>
 	</div>
