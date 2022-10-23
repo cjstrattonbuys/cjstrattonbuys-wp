90e43095bc6c0b97809920277f21266ba38da964
CJ Buys <cjstrattonbuys@gmail.com>
2022-10-23 12:13:00 -0400

    Added spacing for list items

--- front-page.php
+++ front-page.php
@@ -13,7 +13,7 @@ get_header(); ?>
 	<div class="main-content">
 		<div class="content">
 			<?php while ( have_posts() ): the_post(); ?>
-				<h2><?php the_title(); ?></h2>
+				<h1><?php the_title(); ?></h1>
 				<?php the_content(); ?>
 			<?php endwhile; ?>
 		</div>
